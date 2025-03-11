16fe779dfdaba3af2fd2ba80a2e215a571718f58
Elgydium <v4lj4.eemeli@gmail.com>
2025-03-11 21:13:42 +0200

    Remove testing job position

--- src/lib/data/timelineitems.ts
+++ src/lib/data/timelineitems.ts
@@ -7,20 +7,4 @@ export const timelineItems = [
         description: "Game Development and Web Development.",
         skills: ["Web Development", "Svelte", "TypeScript", "Game Development", "Godot"]
     },
-    {
-        title: "System Administrator (Internship)",
-        company: "Päivölä Institute",
-        startDate: new Date("2024-05-01"),
-        endDate: null,
-        description: "Managing Ubuntu servers, handling identity & access management with Keycloak and using Ansible to automate processes.",
-        skills: ["Linux", "Ubuntu", "Keycloak", "Ansible", "Networking", "IT-Support"]
-    },
-    {
-        title: "System Administrator (Internship)",
-        company: "Päivölä Institute",
-        startDate: new Date("2020-05-01"),
-        endDate: null,
-        description: "Managing Ubuntu servers, handling identity & access management with Keycloak and using Ansible to automate processes.",
-        skills: ["Linux", "Ubuntu", "Keycloak", "Ansible", "Networking", "IT-Support"]
-    },
 ];
